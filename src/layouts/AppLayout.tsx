dd7f0819c3de6b3488c787bc206e905dd416c674
misterdan100 <danielmca60@gmail.com>
2024-09-21 18:17:48 -0500

    core: create vite project, set routing, and create main layout component

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router-dom"
+import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
+
+export default function AppLayout() {
+  return (
+    <>
+        <header
+            className="bg-gray-800 py-5"
+        >
+            <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                <div className="w-64">
+                    <Logo />
+                </div>
+
+                <NavMenu />
+
+                
+            </div>
+
+        </header>
+        <section className="max-w-screen-2xl mx-auto mt-5 p-5">
+            <Outlet />
+        </section>
+
+        <footer className="py-5 ">
+            <p className="text-center">
+                Todos los derechos reservados {new Date().getFullYear()}
+            </p>
+            <p className="text-center italic text-gray-400 transition">
+                Coded by 
+                <a href="https://github.com/misterdan100" target="_blank" className="text-sky-500 italic hover:text-sky-300"> Daniel Caceres</a>
+            </p>
+
+        </footer>
+    </>
+  )
+}
